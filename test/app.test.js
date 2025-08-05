fca2a3e3e296dab6b8f158d7cad16476bbcac04d
Abijoshi <abijoshi86@gmail.com>
2025-08-05 15:02:25 +0530

    Fix: added expect import in test

--- test/app.test.js
+++ test/app.test.js
@@ -1,10 +1,17 @@
 const request = require('supertest');
-const app = require('../src/index');
+const app = require('../app');
+const expect = require('chai').expect;
 
-describe('GET /', () => {
-  it('should return status 200 and text', async () => {
-    const res = await request(app).get('/');
-    expect(res.statusCode).toEqual(200);
-    expect(res.text).toContain('Intermediate Jenkins pipeline');
+describe('GET /', function () {
+  it('should return status 200 and text', function (done) {
+    request(app)
+      .get('/')
+      .expect(200)
+      .expect('Content-Type', /text/)
+      .end(function (err, res) {
+        if (err) return done(err);
+        expect(res.text).to.equal('Hello, Jenkins!');
+        done();
+      });
   });
 });
